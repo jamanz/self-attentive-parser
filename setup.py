0db8061f2652e556f8e72fcd25a7ca4e5c8f8271
Потрок Покрохи <45261551+jamanz@users.noreply.github.com>
2025-06-08 03:48:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setuptools.setup(
     name="benepar",
-    version="0.2.0",
+    version="0.2.1",
     author="Nikita Kitaev",
     author_email="kitaev@cs.berkeley.edu",
     description="Berkeley Neural Parser",
